9d41e582663893c8e944b71c26e271964f654c82
SergeMaikere <serge.maikere@gmail.com>
2025-12-10 12:07:11 +0100

    + Gun follows mouse

--- Utils/Gun.py
+++ Utils/Gun.py
@@ -1,3 +1,4 @@
+from typing import Any
 from settings import *
 from pygame.sprite import Group
 from Utils.Sprite import Sprite
@@ -7,6 +8,7 @@ from Utils.Loader import image_loader
 
 class Gun ( Sprite ):
 	def __init__(self, group: Group | tuple[Group, ...], player: Player):
+
 		self.player = player
 		self.player_distance = 140
 		self.player_direction = pygame.Vector2(1,0)
@@ -16,3 +18,15 @@ class Gun ( Sprite ):
 			image_loader(join('assets', 'images', 'gun'), 'gun.png'), 
 			center= self.player.rect.center + self.player_direction * self.player_distance
 		)
+
+	def __get_direction ( self ):
+		mouse_position = pygame.Vector2( pygame.mouse.get_pos() )
+		player_position = pygame.Vector2( (WINDOW_WIDTH/2, WINDOW_HEIGHT/2) )
+		self.player_direction = (mouse_position - player_position).normalize()
+
+	def __move ( self ):
+		self.rect.center = self.player.rect.center + self.player_direction * self.player_distance
+
+	def update ( self, give: dict[str, Any] ):
+		self.__get_direction()
+		self.__move()
