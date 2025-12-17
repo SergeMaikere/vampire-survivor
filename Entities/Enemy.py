d5a41cd1166d8a775e0390f52ca6bbe48aeb3317
SergeMaikere <serge.maikere@gmail.com>
2025-12-17 20:32:50 +0100

    + enemy reactive to collision sprites

--- Entities/Enemy.py
+++ Entities/Enemy.py
@@ -13,7 +13,7 @@ class Enemy ( Sprite ):
 			image_loader(join('assets', 'images', 'enemies', type), '0.png'), 
 			center = get_random_pos(dimensions[0], dimensions[1])
 		)
-
+		self.hitbox_rect = self.rect.inflate(-50, -50)
 		self.player = player
 		self.speed = 100
 		self.direction = pygame.Vector2()
@@ -21,9 +21,33 @@ class Enemy ( Sprite ):
 	def __get_direction ( self ):
 		self.direction = (pygame.Vector2(self.player.rect.center) - pygame.Vector2(self.rect.center)).normalize()
 
-	def __move ( self, dt: float ):
-		self.rect.center += self.direction * self.speed * dt
+	
+	def __collision_handler ( self, collision_sprites: Group, direction: str ):
+		for sprite in collision_sprites:
+			if sprite.rect.colliderect(self.hitbox_rect):
 
+				if direction == 'horizontal':
+					if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
+					if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+
+				if direction == 'vertical':
+					if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
+					if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
+
+
+	def __on_collide ( self, dt: float, collision_sprites: Group ):
+		self.hitbox_rect.x += self.direction.x * self.speed * dt
+		self.__collision_handler(collision_sprites, 'horizontal')
+
+		self.hitbox_rect.y += self.direction.y * self.speed * dt
+		self.__collision_handler(collision_sprites, 'vertical')
+
+
+	def __move ( self ):
+		self.rect.center = self.hitbox_rect.center
+
+	
 	def update ( self, give: dict[str, Any] ):
 		self.__get_direction()
-		self.__move(give['dt'])
+		self.__on_collide(give['dt'], give['groups']['collision_sprites'])
+		self.__move()
